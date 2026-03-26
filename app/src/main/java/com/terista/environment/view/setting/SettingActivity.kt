9939bee8332af429a0ee76c3c358327d41bdbdc0
Saurav Singha <sauravsingha1000@gmail.com>
2026-03-26 18:29:04 +0530

    Initial upload

---
+++ app/src/main/java/com/terista/environment/view/setting/SettingActivity.kt
@@ -0,0 +1,32 @@
+package com.terista.environment.view.setting
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import com.terista.environment.R
+import com.terista.environment.databinding.ActivitySettingBinding
+import com.terista.environment.util.inflate
+import com.terista.environment.view.base.BaseActivity
+
+class SettingActivity : BaseActivity() {
+
+    private val viewBinding: ActivitySettingBinding by inflate()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(viewBinding.root)
+        initToolbar(viewBinding.toolbarLayout.toolbar, R.string.setting, true)
+        supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment, SettingFragment())
+                .commit()
+    }
+
+    companion object{
+        fun start(context: Context){
+            val intent = Intent(context,SettingActivity::class.java)
+            intent.action = Intent.ACTION_OPEN_DOCUMENT
+            context.startActivity(intent)
+        }
+    }
+
+}
